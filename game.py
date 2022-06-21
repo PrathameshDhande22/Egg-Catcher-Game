8f381d6631e51865a4e1ba2f82ede77f3e162b53
Prathamesh Dhande <prathameshdhande5139@gmail.com>
2022-06-21 20:13:19 +0530

    Added the motion to the egg

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import pygame
 from pygame.locals import *
 import random
@@ -15,6 +16,14 @@ class Game:
         self.score=0
         self.basket_x=self.screen_width/2
         self.basket_y=600
+        self.egg_x=0
+        self.egg_y=0
+        self.count=0
+        self.vy=4
+
+        self.hen_no=random.randint(1,5)
+        print(self.hen_no)
+        # self.hen_no=5
 
         self.window=pygame.display.set_mode((self.screen_width,self.screen_height))
         self.icon_img=pygame.image.load("Images/g_icon.png")
@@ -31,7 +40,12 @@ class Game:
         while not self.exit_game:
             self.clock.tick(40)
             self.screen_placer(self.back_img,0,0)
+            if self.count==0:
+                self.set_egg()
+                self.count=1
+            self.loop_egg(self.egg_x,self.egg_y)
             self.screen_placer(self.basket_img,self.basket_x,self.basket_y)
+            
             self.screen_placer(self.score_img,0,0)
             self.set_live()
             self.set_hens()
@@ -47,11 +61,13 @@ class Game:
             elif self.key_pressed[K_RIGHT]:
                 self.basket_x+=7
 
+            # if the basket gets behind the walls so this is set
             if self.basket_x<0:
                 self.basket_x=0
             elif self.basket_x>818:
                 self.basket_x=818
 
+            self.egg_y+=self.vy
             pygame.display.update()
 
     def __load_image(self):
@@ -61,7 +77,7 @@ class Game:
 
         # loads the basket image
         self.basket_img=pygame.image.load("Images/basket.png")
-        self.basket_img=pygame.transform.scale(self.basket_img,(80,80))
+        self.basket_img=pygame.transform.scale(self.basket_img,(90,90))
 
         # loads the egg_cracked image
         self.eggcracked_img=pygame.image.load("Images/egg_cracked.png")
@@ -69,7 +85,7 @@ class Game:
 
         # loads the egg image
         self.egg_img=pygame.image.load("Images/egg.png")
-        self.egg_img=pygame.transform.scale(self.egg_img,(40,40))
+        self.egg_img=pygame.transform.scale(self.egg_img,(45,60))
 
         # loads the hen image
         self.hen_img=pygame.image.load("Images/hen.png")
@@ -81,7 +97,9 @@ class Game:
 
         # loads the life image
         self.life_img=pygame.image.load("Images/life.png")
-        self.life_img=pygame.transform.scale(self.life_img,(40,40))
+        self.life_img=pygame.transform.scale(self.life_img,(40,50))
+
+
 
     def set_live(self):
         if self.life==3:
@@ -98,13 +116,38 @@ class Game:
 
     def set_hens(self):
         pygame.draw.line(self.window,(150,75,0),(0,155),(900,155),width=30)
-        self.screen_placer(self.hen_img,20,70)
-        self.screen_placer(self.hen_img,190,70)
-        self.screen_placer(self.hen_img,360,70)
-        self.screen_placer(self.hen_img,530,70)
-        self.screen_placer(self.hen_img,700,70)
-        
+        self.screen_placer(self.hen_img,20,70) # hen 1
+        self.screen_placer(self.hen_img,190,70) # hen 2
+        self.screen_placer(self.hen_img,360,70) # hen 3
+        self.screen_placer(self.hen_img,530,70) # hen 4
+        self.screen_placer(self.hen_img,700,70) # hen 5
 
+    def set_egg(self):
+        if self.hen_no==1:
+            self.egg_x=60
+            self.egg_y=170
+            
+        elif self.hen_no==2:
+            self.egg_x=230
+            self.egg_y=170
+            
+        elif self.hen_no==3:
+            self.egg_x=400
+            self.egg_y=170
+            
+        elif self.hen_no==4:
+            self.egg_x=570
+            self.egg_y=170
+            
+        elif self.hen_no==5:
+            self.egg_x=740
+            self.egg_y=170
+            
+        print(f"Hen no: {self.hen_no} x={self.egg_x} y={self.egg_y}")
+        
+    def loop_egg(self,x,y):
+        # time.sleep(0.09)
+        self.screen_placer(self.egg_img,x,y)
 
 
 
