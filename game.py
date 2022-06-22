849bec6580aa9b54c43ec656952a127070bfd719
Prathamesh Dhande <prathameshdhande5139@gmail.com>
2022-06-22 12:14:46 +0530

    Added the game over screen and high score screen

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
 import sys
 import threading
-import time
 import pygame
 from pygame.locals import *
 import random
+import configparser
 
 class Game:
     def __init__(self):
@@ -20,9 +20,15 @@ class Game:
         self.egg_x=0
         self.egg_y=0
         self.count=0
-        self.vy=6
+        self.vy=7
         self.text=pygame.font.SysFont("papyrus",50,True)
+        self.text1=pygame.font.SysFont("papyrus",90,True)
+        self.text2=pygame.font.SysFont("forte",40)
         self.crack=False
+        self.coord=[]
+        self.game_over=False
+        self.high_score=0
+        self.conti=False
 
         self.hen_no=random.randint(1,5)
         # self.hen_no=5
@@ -41,43 +47,69 @@ class Game:
     def __gameloop__(self):
         while not self.exit_game:
             self.clock.tick(40)
-            self.screen_placer(self.back_img,0,0)
-            if self.count==0:
-                self.set_egg()
-                self.count=1
-            threading.Thread(target=self.loop_egg).start()
-            self.screen_placer(self.basket_img,self.basket_x,self.basket_y)
-            
-            self.screen_placer(self.score_img,0,0)
-            self.set_live()
-            self.set_hens()
-            for self.event in pygame.event.get():
-                if self.event.type==pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-
-            self.key_pressed=pygame.key.get_pressed()
-            if self.key_pressed[K_LEFT]:
-                self.basket_x+=-8
+            self.get_highscore()
+            if self.game_over:
                 
-            elif self.key_pressed[K_RIGHT]:
-                self.basket_x+=8
+                if int(self.high_score)<=self.score:
+                    self.window.fill((21, 254, 12))
+                    self.screen_placer(self.highscore_img,150,50)
+                    self.highscore_text=self.text1.render(str(self.high_score),True,(0,0,0))
+                    self.screen_placer(self.highscore_text,450,400)
+                    self.info_text=self.text.render("Press Enter to Continue",True,(0,0,0))
+                    self.screen_placer(self.info_text,150,580)
 
-            # if the basket gets behind the walls so this is set
-            if self.basket_x<0:
-                self.basket_x=0
-            elif self.basket_x>818:
-                self.basket_x=818
-            self.check_collison()
+                if self.conti:
+                    self.game_over_screen()
+                
+                else:
+                    self.game_over_screen()
+                
+                for self.event in pygame.event.get():
+                    if self.event.type==pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
+                    
+                    if self.event.type==pygame.KEYDOWN:
+                        if self.event.key==pygame.K_RETURN:
+                            self.conti=True
+                            
 
-            # if egg is cracked then the image should appear
-            # if self.crack:
-            #     self.screen_placer(self.eggcracked_img,self.egg_x,self.egg_y)
-            # else:
-            #     self.screen_placer(self.egg_img,self.egg_x,self.egg_y)
-            self.score_text=self.text.render(str(self.score),True,(0,0,0))
-            self.screen_placer(self.score_text,140,-15)
-            self.egg_y+=self.vy
+
+            else:
+            
+                self.screen_placer(self.back_img,0,0)
+                if self.count==0:
+                    self.set_egg()
+                    self.count=1
+                threading.Thread(target=self.loop_egg).start()
+                self.screen_placer(self.basket_img,self.basket_x,self.basket_y)
+                
+                self.screen_placer(self.score_img,0,0)
+                self.set_live()
+                self.set_hens()
+                for self.event in pygame.event.get():
+                    if self.event.type==pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
+
+                self.key_pressed=pygame.key.get_pressed()
+                if self.key_pressed[K_LEFT]:
+                    self.basket_x+=-8
+                    
+                elif self.key_pressed[K_RIGHT]:
+                    self.basket_x+=8
+
+                # if the basket gets behind the walls so this is set
+                if self.basket_x<0:
+                    self.basket_x=0
+                elif self.basket_x>818:
+                    self.basket_x=818
+                self.check_collison()
+
+                self.set_cracked_get()
+                self.score_text=self.text.render(str(self.score),True,(0,0,0))
+                self.screen_placer(self.score_text,140,-15)
+                self.egg_y+=self.vy
             pygame.display.update()
 
     def __load_image(self):
@@ -91,7 +123,7 @@ class Game:
 
         # loads the egg_cracked image
         self.eggcracked_img=pygame.image.load("Images/egg_cracked.png")
-        self.eggcracked_img=pygame.transform.scale(self.eggcracked_img,(60,60))
+        self.eggcracked_img=pygame.transform.scale(self.eggcracked_img,(120,100))
 
         # loads the egg image
         self.egg_img=pygame.image.load("Images/egg.png")
@@ -109,6 +141,14 @@ class Game:
         self.life_img=pygame.image.load("Images/life.png")
         self.life_img=pygame.transform.scale(self.life_img,(40,50))
 
+        # loads the gameover image
+        self.gameover_img=pygame.image.load("Images/game_over.jpeg")
+        self.gameover_img=pygame.transform.scale(self.gameover_img,(600,400))
+
+        # loads the highscore image
+        self.highscore_img=pygame.image.load("Images/highscore.jpeg")
+        self.highscore_img=pygame.transform.scale(self.highscore_img,(600,400))
+
 
 
     def set_live(self):
@@ -123,14 +163,21 @@ class Game:
 
         elif self.life==1:
             self.screen_placer(self.life_img,850,6)
+        
+        elif self.life==0:
+            self.game_over=True
 
     def set_hens(self):
-        pygame.draw.line(self.window,(150,75,0),(0,155),(900,155),width=30)
-        self.screen_placer(self.hen_img,20,70) # hen 1
-        self.screen_placer(self.hen_img,190,70) # hen 2
-        self.screen_placer(self.hen_img,360,70) # hen 3
-        self.screen_placer(self.hen_img,530,70) # hen 4
-        self.screen_placer(self.hen_img,700,70) # hen 5
+        if self.life==0:
+            self.game_over=True
+        else:
+                
+            pygame.draw.line(self.window,(150,75,0),(0,155),(900,155),width=30)
+            self.screen_placer(self.hen_img,20,70) # hen 1
+            self.screen_placer(self.hen_img,190,70) # hen 2
+            self.screen_placer(self.hen_img,360,70) # hen 3
+            self.screen_placer(self.hen_img,530,70) # hen 4
+            self.screen_placer(self.hen_img,700,70) # hen 5
 
     def set_egg(self):
         if self.hen_no==1:
@@ -162,8 +209,11 @@ class Game:
     def check_collison(self):
         self.egg_rect=pygame.Rect(self.egg_x,self.egg_y,45,60)
         self.basket_rect=pygame.Rect(self.basket_x+30,self.basket_y+40,50,70)
-        if self.egg_y>self.screen_height-60:
+        if self.egg_y>self.screen_height-70:
             self.crack=True
+            self.coord.insert(0,self.egg_x)
+            self.coord.insert(1,self.egg_y)
+            del self.coord[2:]
             self.count=0
             self.hen_no=random.randint(1,5)
             self.life-=1
@@ -175,7 +225,30 @@ class Game:
             self.count=0
             self.egg_y=700
         
-      
+    def set_cracked_get(self):
+        if self.crack:
+            self.screen_placer(self.eggcracked_img,self.coord[0],self.coord[1]-20)
+
+    def get_highscore(self):
+        config=configparser.ConfigParser()
+        config.read("config.ini")
+        self.high_score=config['score']['highscore']
+        if int(self.high_score)<self.score:
+            self.high_score=self.score
+            with open("config.ini","w") as f:
+                f.write(f"[score]\nhighscore={self.high_score}")
+
+    def game_over_screen(self):
+        self.window.fill((0,0,0))
+        self.screen_placer(self.gameover_img,150,50)
+        self.txt1=self.text2.render("Press 1 to Play Again",True,(255,255,255))
+        self.txt2=self.text2.render("Press 2 for Main Menu",True,(255,255,255))
+        self.txt3=self.text2.render("Press 3 to Exit",True,(255,255,255))
+        self.screen_placer(self.txt1,275,400)
+        self.screen_placer(self.txt2,275,450)
+        self.screen_placer(self.txt3,275,500)
+
+        
 
 
 
